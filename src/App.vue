471d203b54ef6b28233f5e6b3151ef9b9b4c33c7
prolixmagus <jeremypfishman@gmail.com>
2024-07-01 11:28:28 -0700

    Add contact view

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <script setup>
   import { RouterLink, RouterView } from 'vue-router'
-  import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/contact">Contact</RouterLink>
       </nav>
     </div>
   </header>
 
-  <RouterView />
+  <main class="outlet">
+    <RouterView />
+  </main>
+
+  <footer>I am a footer</footer>
 </template>
 
 <style scoped>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,8 @@
+<template>
+	<section>
+		<h1>Contact Page</h1>
+		<p>This is the contact page</p>
+	</section>
+</template>
+
+<style></style>
